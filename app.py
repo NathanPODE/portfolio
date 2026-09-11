a96a936f8e09e4a5bf9ec030cdc9ce1933d9285c
NathanPODE <261354437+NathanPODE@users.noreply.github.com>
2026-09-11 10:06:30 -0700

    Allows for website to be accessed across my network, added about section and changed padding

--- app.py
+++ app.py
@@ -2,14 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html", projects=projects)
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-
-
 projects = [
     {
         "title": "Project One",
@@ -24,3 +16,12 @@ projects = [
         "image": "link",
     },
 ]
+
+@app.route("/")
+def home():
+    return render_template("index.html", projects=projects)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+
